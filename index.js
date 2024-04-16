d7599d7431e43cb93c8464f4bd5d73ceb7301f9f
nstygurl <111138043+nstygurl@users.noreply.github.com>
2024-04-16 09:48:44 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,201 @@
+let levels = [
+    ["What do you think is the hardest part of what I do for a living?",
+    "What was the first thing you noticed about me?",
+    "If Myspace was still a thing, what would my profile song be?",
+    "If you were to buy me a present, knowing nothing about me other than what I look like, what would it be?",
+    "Do you think plants thrive or die in my care? Explain.",
+    "Do I look kind? Explain.",
+    "Do I seem like more of a creative or analytical type? Explain.",
+    "Finish the sentence: Just by looking at you, I'd think _____________.",
+    "What do my shoes tell you about me?",
+    "What was your first impression of me?",
+    "How would you describe what you think my type is in three words?",
+    "Between the two of us, who seems like the better texter? Explain.",
+    "Make an assumption about me",
+    "What about me intrigues you?",
+    "What do you think I'm most likely to splurge on?",
+    "As a child, what do you think I wanted to be?",
+    "Do you think I was popular in school? Explain.",
+    "Do you think I fall in love easily why or why not",
+    "What compliment do you think I hear the most?",
+    "Do you think I'm usually early on time or late to events. Explain.",
+    "Do I remind you of anyone you know?",
+    "Am I someone you typically connect with?",
+    "Do I intimidate you? Why or why not?",
+    "What's your favorite song lyric that comes to your mind from the top of your head?",
+    "How important do you think birthdays and holidays are to me? Explain.",
+    "Who do you think was more nervous on our first date? Explain.",
+    "What do you think my perfect date night would be?",
+    "What assumption did you make about me that turned out to be false?",
+    "On a scale of 1-10, how open do you feel I am with you? Explain.",
+    ],
+    ["What's been your happiest memory this past year?",
+    "Have you changed your mind about anything recently?",
+    "What has been your earliest recollection of happiness?",
+    "What lesson took you the longest to unlearn?",
+    "Are you lying to yourself about anything?",
+    "What questions are you trying to answer most in your life right now?",
+    "When was the last time you surprised yourself?",
+    "What title would you give this chapter in your life?",
+    "What do you crave more of?",
+    "Finish the sentence: Strangers would describe me as ______ only I know that I am _______",
+    "What's the worst pain you have ever been in that wasn't physical?",
+    "Has a stranger ever changed your life?",
+    "What's your first loves name and the reason you fell in love with them?",
+    "What would your younger self not believe about your life today?",
+    "How would you describe the feeling of being in love with one word?",
+    "What are you more afraid of: failure or success, and why?",
+    "What is a dream you let go of?",
+    "If you could get to know someone in your life on a deeper level, who would it be and why?",
+    "What's your mom's name, and what is the most beautiful thing about her?",
+    "What part of your life works what part of your life hurts?",
+    "How can you become a better person?",
+    "Do you think the image you have of yourself matches the image other people see you as?",
+    "Are you missing anyone right now? Do you think they miss you too?",
+    "What fast food restaurant do you think I'm most likely to drive through? What's my order?",
+    "Do you think I intimidate others? Why or why not?",
+    "When you are asked how are you, how often do you answer truthfully?",
+    "How are you, really?",
+    "What's the most unexplainable thing that happened to you?",
+    "What's your father's name? And tell me one thing about him.",
+    "If you could have it your way: who would you be with? Where would you be? What would you be doing?",
+    "Describe your perfect day!",
+    "What dating advice would you give your younger self?",
+    "How old do you feel emotionally?",
+    "Will I find your Ex on your Instagram feed? Why or why not?",
+    "What's the most embarrassing thing that happened to you on a date?",
+    "What could you have done better in previous relationships?",
+    "Is there an image of yourself you try to project on a first date you wish you could let go of?",
+    "Finish the sentence: Dating is ____________",
+    "What did the people who raised you teach you about love?",
+    "Describe your perfect date.",
+    "What has being single taught you about yourself?",
+    "What's your least favorite question to be asked on a date? What do you wish you would be asked more?",
+    "What are you passionate about?",
+    "What are you not currently giving enough time to?",
+    "The hardest thing for me to reveal about myself to you was ________.",
+    "How does one earn your trust? Have I earned it? How can I earn more?",
+    "What has this relationship taught you about yourself?",
+    "What’s the most romantic thing I’ve done for you recently?",
+    "What’s one small way I can be a better partner?",
+    "What’s the most attractive thing I do without realizing it?",
+    "What did our worst argument teach you?",
+    "What are you currently working through that I don’t see, if anything?",
+    "What song best describes our relationship?",
+    "Do you think my job affects me positively or negatively? How does it affect us?",
+    "What have you been extra sensitive too lately? ",
+    "What feelings are hard for you to communicate to me? How can I make it easier?",
+    "What immediately attracted you to me? What attracted you more overtime?",
+    "What’s the best gift I’ve given you? Material and immaterial.",
+    "What about you feel hardest to love?",
+    "What do you wish we did more of?",
+    "What made you fall in love with me?",
+    "Whats the most important lesson a past relationship has taught you?",
+    "Finish the sentence: Thank you for accepting______.",
+    "Are there any insecurities from previous relationships that you carried into this one? If so, what are they?",
+    "What recent experience made you feel closer to me?",
+    "How do our strengths and weaknesses complement each other?",
+    "What have I helped you appreciate about yourself?",
+    "How do I show that I love you without telling you?",
+    "When was the last time I hurt you, perhaps unintentionally?",
+    "How have you seen me grow in this relationship?",
+    "Have I helped you change your mind about anything?",
+    "What fear do you think holds me back the most?",
+    "What’s one small way we can improve our sex life?",
+    ],
+    ["What about me surprised you?",
+    "What do you think my superpower is?",
+    "What do you think our most important similarities are?",
+    "What do you think is one thing I could do that would drastically improve my life?",
+    "What would be the perfect gift for me?",
+    "How would you describe me to a stranger?",
+    "What is a lesson you will take away from our conversation?",
+    "What do you think do I fear the most?",
+    "Where am I most qualified to give advice about?",
+    "What about me is the hardest for you to understand?",
+    "If we were a band, what would be our name?",
+    "Admit something.",
+    "What parts of yourself do you see in me?",
+    "How does one earn your vulnerability?",
+    "What do you recommend I should let go of?",
+    "What has this conversation taught you about yourself?",
+    "What do you think my defining characteristic is?",
+    "What question were you most afraid to answer?",
+    "What do you think my weakness is?",
+    "How do our personalities complement each other?",
+    "What do you think I should know about myself that perhaps I'm unaware of?",
+    "What would make you feel closer to me?",
+    "What do you admire most about me?",
+    "In one word, describe how you feel right now.",
+    "What answer of mine made you light up?",
+    "What's the most attractive quality about me that isn't physical?",
+    "Am I like you expected me to be?",
+    "What answer of mine surprised you the most throughout the game, if any?",
+    "Is there anything you still don’t know about me that you wish you did? ",
+    "What goal would feel best for you to accomplish this year?",
+    "What don’t I give myself enough credit for?",
+    "What about our relationship are you proudest of?",
+    "What did this conversation teach you about our relationship? What did it teach you about yourself?",
+    "Thank you for helping me realize_____ about myself.",
+    ],
+];
+
+let wildCards = [
+    "Write down the three most important things to you in a relationship (30 sec) Compare.",
+    "Write down a dating pet peeve (30 sec). Compare.",
+    "Draw your current mood. Compare.",
+    "Dare your partner to do something out of their comfort zone next week.",
+    "Draw your favorite memory with me. Compare.",
+    "Write down one small way you can be better in this relationship. Both players Compare",
+    "Write down the top 3 things your partner does that makes you feel most loved. Both players compare.",
+    "Write down the title you would give this chapter of our relationship, compare.",
+    "Write a short guide of how to love your partner well. 30 seconds. Compare",
+    "Ask your partner something you’ve never asked them before.",
+    "Write down the top 3 you’re most grateful for in your partner, 30 seconds. Compare.",
+    "Write down your intention for our relationship. Compare.",
+];
+
+let currentLevel = 0;
+let questionShown = [];
+let subTitle = ["conection", "reflection"];
+
+$("#card").on("click", function() {
+    showRandomQuestion();
+});
+
+$(document).on("keypress", function() {
+    showRandomQuestion();
+});
+
+function showRandomQuestion() {
+    let currentLevelQuestions = levels[currentLevel];
+    let currentQuestionsIndex;
+    
+    if (questionShown.length === currentLevelQuestions.length) {
+        currentLevel++;
+        questionShown = [];
+        if (currentLevel >= levels.length) {
+            alert("Fin del juego");
+            return;
+        }
+        $("#subtitle").text("Level " + (currentLevel + 1));
+    }
+
+    do {
+        currentQuestionsIndex = Math.floor(Math.random() * currentLevelQuestions.length);
+    } while (questionShown.includes(currentQuestionsIndex));
+
+    const showWilCard = Math.random() < 0.09
+    if (showWilCard) {
+        let wildCardIndex = Math.floor(Math.random() * wildCards.length);
+        $(".question").html("<b>Wild Card:</b><br>" + wildCards[wildCardIndex]);
+        $("#card").removeClass("normal");
+        $("#card").addClass("wild");
+    } else {
+        $(".question").text(currentLevelQuestions[currentQuestionsIndex]);
+        questionShown.push(currentQuestionsIndex);
+        $("#card").removeClass("wild");
+        $("#card").addClass("normal");
+    }
+}
+
